70347c2850384f9ca21ff57cb8135db39dd5a332
gouz7514 <gouz7514@gmail.com>
2021-03-15 17:40:37 +0900

    update Posts에서 목록 불러오기

--- src/main/java/example/org/spring_khj/service/posts/PostsService.java
+++ src/main/java/example/org/spring_khj/service/posts/PostsService.java
@@ -3,12 +3,17 @@ package example.org.spring_khj.service.posts;
 import com.sun.xml.internal.ws.developer.Serialization;
 import example.org.spring_khj.domain.posts.Posts;
 import example.org.spring_khj.domain.posts.PostsRepository;
+import example.org.spring_khj.web.dto.PostsListResponseDto;
 import example.org.spring_khj.web.dto.PostsResponseDto;
 import example.org.spring_khj.web.dto.PostsSaveRequestDto;
 import example.org.spring_khj.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.transaction.TransactionScoped;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +37,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id = " + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
