a9d0654fc7976c34baa1114eb2c3d0af18536260
gouz7514 <gouz7514@gmail.com>
2021-03-15 17:52:09 +0900

    update 게시글 수정 기능 추가

--- src/main/java/example/org/spring_khj/web/IndexController.java
+++ src/main/java/example/org/spring_khj/web/IndexController.java
@@ -1,10 +1,12 @@
 package example.org.spring_khj.web;
 
 import example.org.spring_khj.service.posts.PostsService;
+import example.org.spring_khj.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +23,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
