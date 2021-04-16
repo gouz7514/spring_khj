217ee4d76421c5377413c7990e22083fb0e8afe4
gouz7514 <gouz7514@gmail.com>
2021-04-16 15:58:00 +0900

    update 무중단 배포 스크립트 만들기

--- src/main/java/example/org/spring_khj/config/auth/SecurityConfig.java
+++ src/main/java/example/org/spring_khj/config/auth/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable().headers().frameOptions().disable() // h2-console 화면 사용하기 위해 해당 옵션들을 disable
                 .and()
                     .authorizeRequests() // URL별 권한 관리를 설정하는 옵션의 시작점
-                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
                     .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                     .anyRequest().authenticated()
                 .and()
